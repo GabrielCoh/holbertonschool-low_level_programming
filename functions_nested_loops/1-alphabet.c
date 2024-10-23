c056cac5d55aaedc834d265062365e2d4882fec9
GabrielCoh <9924@holbertonstudents.com>
2024-10-23 08:45:20 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print the alphabet
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	_putchar(a);
+	_putchar('\n');
+	return (0);
+}
