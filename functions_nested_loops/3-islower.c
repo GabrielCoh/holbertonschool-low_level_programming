b0e65b9321a180872f4a3108ec8d4faf009a344a
GabrielCoh <9924@holbertonstudents.com>
2024-10-23 09:59:27 +0000

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main."
+
+/**
+ * main - Check for lowercase letters
+ *
+ * Return: Always o (Success)
+ */
+
+int main(void)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
