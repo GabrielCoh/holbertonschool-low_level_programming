4d16dfcbb8fa5cfe419d91cf13cbb7a7fc7aef2a
GabrielCoh <9924@holbertonstudents.com>
2024-10-24 12:13:42 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integrer.
+ * @c: The number to be computed.
+ * Return: Absolute value of the number or zero.
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * val;
+		return (abs_val);
+	}
+	return (c);
+}
