d17ad03b29d7614543ef7f66bfbdee341b648485
root <GabrielCoh>
2024-10-25 08:30:01 +0000

    24

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -14,16 +14,19 @@ void jack_bauer(void)
 	{
 		for (b = '0'; b <= '9'; b++)
 		{
-			for (c = '0'; c <= '5'; c++)
+			if ((a <= '1' && b <= '9') || (a <= '2' && b <= '4'))
 			{
-				for (d = '0'; d <= '9'; d++)
+				for (c = '0'; c <= '5'; c++)
 				{
-					_putchar(a);
-					_putchar(b);
-					_putchar(58);
-					_putchar(c);
-					_putchar(d);
-					_putchar('\n');
+					for (d = '0'; d <= '9'; d++)
+					{
+						_putchar(a);
+						_putchar(b);
+						_putchar(58);
+						_putchar(c);
+						_putchar(d);
+						_putchar('\n');
+					}
 				}
 			}
 		}
