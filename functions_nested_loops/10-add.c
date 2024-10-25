67036986455817c70c15111053f4d53592231e2c
root <GabrielCoh>
2024-10-25 11:14:36 +0000

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * add - Adds two values
+  * @n: The first number to be computed
+  * @m: The second number to be computed
+  *
+  * Return: Integer value
+  */
+
+int add(int n, int m)
+{
+	return (n + m);
+}
