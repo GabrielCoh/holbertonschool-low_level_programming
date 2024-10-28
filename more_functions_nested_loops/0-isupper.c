17ba0ef7942a881bb0d0a54cbec8318af766425c
GabrielCoh <9924@holbertonstudents.com>
2024-10-28 08:42:19 +0000

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * isupper - Check for uppercase character
+ *@c: character to be checked
+ * Return: 1 if uppercase and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
