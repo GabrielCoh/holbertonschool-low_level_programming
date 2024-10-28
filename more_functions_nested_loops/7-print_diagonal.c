1ab16605bb6626934f89b252bca4a67131eb5782
GabrielCoh <9924@holbertonstudents.com>
2024-10-28 13:15:10 +0000

    diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draw a diagonal line in the terminal
+ * @n: The number of lines to draw
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			_putchar(92);
+		}
+		_putchar('\n');
+	}
+}
