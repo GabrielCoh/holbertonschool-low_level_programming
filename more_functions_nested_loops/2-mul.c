fa034a3de414fd5aeeb4c4bd600bfc304e119589
GabrielCoh <9924@holbertonstudents.com>
2024-10-28 09:20:31 +0000

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integrers
+ * @a: First integrer
+ * @b: second integrer
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	for (a * b);
+	return (0);
+}
