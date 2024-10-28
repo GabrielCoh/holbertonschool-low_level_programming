d2342916c47e76c4b979f23492ae1c5a9fc8ec8c
GabrielCoh <9924@holbertonstudents.com>
2024-10-28 14:18:08 +0000

    #

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Print a square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+                }
+        }
+}
