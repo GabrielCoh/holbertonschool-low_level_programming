0920e5b3def9aacd7f43956b3884afbb2a5064bc
GabrielCoh <9924@holbertonstudents.com>
2024-10-28 09:44:30 +0000

    09

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * print_numbers - Print numbers from 0 to 9
- * Return: Always 0
+ * Return: void
  */
 
 void print_numbers(void)
@@ -12,5 +12,4 @@ void print_numbers(void)
 	for (a >= '0'; a <= '9'; a++)
 	_putchar('a');
 	_putchar('\n');
-	return (0);
 }
