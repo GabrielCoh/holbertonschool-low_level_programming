c25483a59ba927d83df3607ff7fe4d60b4c4379b
GabrielCoh <9924@holbertonstudents.com>
2024-10-28 12:36:58 +0000

    line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Draw a straight line in the terminal
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (a <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; n < n; a++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
