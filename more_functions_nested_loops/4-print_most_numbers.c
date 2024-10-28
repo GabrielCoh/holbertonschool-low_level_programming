cd86b8a0ee3f83a49cab5b3e84f76aef3552dd30
GabrielCoh <9924@holbertonstudents.com>
2024-10-28 10:15:29 +0000

    most

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char a;
+	{
+		for (a = 48; a = 49; a = 51; a <= 57; a++)
+			_putchar(a);
+	}
+	_putchar('\n');
+}
