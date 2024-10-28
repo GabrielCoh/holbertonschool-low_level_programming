cb51b8a29fe48de321a3468fb91ce633e4f640c1
GabrielCoh <9924@holbertonstudents.com>
2024-10-28 10:30:54 +0000

    most

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,8 +9,7 @@ void print_most_numbers(void)
 {
 	char a;
 	{
-		for (a = 48; a <= 49; a++)
-		for (a = 51; a <= 57; a++)
+		for ((a = 48; a <= 49; a++) && (; a = 51; a++) && (a = 53; a <= 57; a++))
 			_putchar(a);
 	}
 	_putchar('\n');
