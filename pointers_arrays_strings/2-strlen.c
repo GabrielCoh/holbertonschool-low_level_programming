61bd3983c64a226dcfcb8ce075714a8f84474813
GabrielCoh <9924@holbertonstudents.com>
2024-10-29 10:05:52 +0000

    strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a string
+ * @s: pointer of the string
+ * Return: O
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+	while (*s != 0)
+	{
+		count++;
+		s++;
+	}
+	return (0);
+}
