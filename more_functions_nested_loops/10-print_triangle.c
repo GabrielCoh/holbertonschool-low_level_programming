6fc2840ae995f701c52218ff481567b2fd387458
GabrielCoh <9924@holbertonstudents.com>
2024-10-29 08:40:09 +0000

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,25 +8,28 @@
 
 void print_triangle(int size)
 {
-	int a, b, c;
+	int a, b;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (size > 0)
 	{
 		for (a = 0; a < size; a++)
 		{
-			for (b = size - a; b < 1; b--)
+			for (b = 0; b < size; b++)
 			{
-				_putchar(32);
-			}
-			for (c = 0; c <= a; c++)
-			{
-				_putchar(35);
+				if (b < size - (a + 1))
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
 			}
 			_putchar('\n');
 		}
+	}
+	else
+	{
+		_putchar ('\n');
 	}
 }
