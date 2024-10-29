bdd1c1198ed95e13629489c5cf67615f7825f3c2
GabrielCoh <9924@holbertonstudents.com>
2024-10-29 15:04:25 +0000

    puts

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Print every other character
+ * @str: pointer
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+			_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
