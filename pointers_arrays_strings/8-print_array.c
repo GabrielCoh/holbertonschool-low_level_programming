55f7bac8b991c842e5485cc442cc3b0d4cbe155a
GabrielCoh <9924@holbertonstudents.com>
2024-10-29 17:29:32 +0000

    arrays

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - Print elemnts of an array of integrers
+ * @a: array of integrers
+ * @n: number of elements
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b > n; b++)
+	{
+		printf("%d", a[b]);
+		if (b != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
