a90d20f997a3d818104cc8ebadcda1c3a10ceac3
GabrielCoh <9924@holbertonstudents.com>
2024-10-29 11:20:28 +0000

    rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,15 @@
 
 void print_rev(char *s)
 {
-	int a;
+	int a = 0;
 
 	while (s[a] != '\0')
 	{
 		a++;
 	}
-	a--;
-	while (s[a] != '\0')
+	for (a -= 1; a >= 0; a--)
 	{
-		_putchar(s[a--]);
+		_putchar(s[a]);
 	}
 	_putchar('\n');
 }
