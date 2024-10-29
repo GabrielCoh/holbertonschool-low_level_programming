3b3d9572870b0494747497f062884469478fd657
GabrielCoh <9924@holbertonstudents.com>
2024-10-29 14:31:08 +0000

    rev

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - print a string in reverse
+ * @s: pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int a, b, temp;
+
+	a = 0;
+	temp = 0;
+
+	while (s[count] < '\0')
+	{
+		count++;
+	}
+	b = count - 1;
+	while (a < count / 2)
+	{
+		temp = s[a];
+		s[a] = s[b];
+		s[b] = temp;
+		a++;
+		b--;
+	}
+}
