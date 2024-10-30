71bb54ffa05332abb5567040d11b4b25cfd2d592
GabrielCoh <9924@holbertonstudents.com>
2024-10-30 12:04:01 +0000

    strcat

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -16,7 +16,7 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-char *_strcat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _stramp(char *s1, char *s2);
 char string_toupper(char *);
