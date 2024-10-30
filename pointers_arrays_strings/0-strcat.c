ce10791cf6da2b83c678095ee7a5d7b03ca08168
GabrielCoh <9924@holbertonstudents.com>
2024-10-30 11:58:20 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main."
+
+/**
+ * _strcat - Concatenate two strings
+ * @dest: pointer argument
+ * @src: pointer argument
+ * Return: string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int _putchar();
 void reset_to_98(int *n);
@@ -12,5 +15,13 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+char *strcat(char *dest, char *src);
+char *strcat(char *dest, char *src, int n);
+char strncpy(char *dest, char *src, int n);
+int _stramp(char *s1, char *s2);
+char string_toupper(char *);
+char char *cap_string(char *);
+char *leet(char *);
+void reverse_array(int *a, int n);
 
 #endif
