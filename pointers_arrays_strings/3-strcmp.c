cf5a191d64f4556de627f590c08dd680b47e28e8
GabrielCoh <9924@holbertonstudents.com>
2024-10-31 11:46:12 +0000

    cmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare two strings
+ * @s1: pointer argument
+ * @s2: pointer argument
+ * Return: _strcmp results
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	for (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+		return (0);
+	}
+}
