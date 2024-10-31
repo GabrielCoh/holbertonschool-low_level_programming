571359d1c40b0053db2c58bef39cb95d254fc760
GabrielCoh <9924@holbertonstudents.com>
2024-10-31 14:12:22 +0000

    cap

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,19 +3,32 @@
 /**
  * *cap_string - Capitalizes all words of a string
  * @s: pointer argument
- * Return: s
+ * Return: Capitalized words
  */
 
 char *cap_string(char *s)
 {
 	int n = 0;
 
-	while (s[n] != '\0')
+	while (s[n])
 	{
-		if (s[n] >= 'a' && s[n] <= 'z')
-		{
-			s[n] += 32;
-		}
+		while (!(s[n] >= 'a' && s[n] <= 'z'))
+			n++;
+		if (s[n - 1] == ' ' ||
+				s[n - 1] == '\t' ||
+				s[n - 1] == '\n' ||
+				s[n - 1] == ',' ||
+				s[n - 1] == '.' ||
+				s[n - 1] == ';' ||
+				s[n - 1] == '!' ||
+				s[n - 1] == '?' ||
+				s[n - 1] == '"' ||
+				s[n - 1] == '(' ||
+				s[n - 1] == ')' ||
+				s[n - 1] == '{' ||
+				s[n - 1] == '}' ||
+				n == 0)
+			s[n] -= 32;
 		n++;
 	}
 	return (s);
