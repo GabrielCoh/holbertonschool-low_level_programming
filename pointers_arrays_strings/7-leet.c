df8ff0bb2ee122bb5cbf2d961c8405bd32676868
GabrielCoh <9924@holbertonstudents.com>
2024-10-31 14:47:44 +0000

    cap

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *leet - Encode a string into 1337
+ * @s: pointer argument
+ * Return: s
+ */
+
+char *leet(char *s)
+{
+	int n, m;
+
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (n = 0; s[n] != '\0'; ++n)
+	{
+		for (m = 0; a[m] != '\0'; m++)
+		{
+			if (s[n] == a[m])
+			{
+				s[n] = b[m / 2];
+			}
+		}
+	}
+	return (s);
+}
