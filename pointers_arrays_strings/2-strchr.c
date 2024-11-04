d3de3fabe97ec63d08747a61f66c270a34b24413
GabrielCoh <9924@holbertonstudents.com>
2024-11-04 14:00:53 +0000

    str

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,16 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
-
-	while (s[i] != '\0')
+	while (*s != '\0' && *s != c)
+		s++;
 	{
-		if (s[i] == c)
+		if (*s == c)
 		{
 			return (s);
 		}
-		i++;
+		else
+		{
+			return (NULL);
+		}
 	}
-	return (NULL);
 }
