48db4d21bd1df83a66a3eeaf3bbdb2373ce1ae4e
GabrielCoh <9924@holbertonstudents.com>
2024-11-14 15:05:26 +0000

    dog

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -5,23 +5,8 @@
 #include <stdlib.h>
 
 /**
- *struct dog - a new type of data
- *representing a dog
- *@name: name of dog
- *@age: age of dog
- *@owner: owner's name
- */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
-
-/**
- *dog_t - Typedef for struct dog
- */
+*dog_t - Typedef for struct dog
+*/
 typedef struct dog dog_t;
 
 
