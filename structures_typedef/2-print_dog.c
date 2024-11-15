30e4a9213387f94d58e1ec736a81124a1b5dd4f8
GabrielCoh <9924@holbertonstudents.com>
2024-11-15 09:24:45 +0000

    print

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -33,11 +33,7 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n", d->age);
 	}
 
-	if ((*d).owner == NULL)
-	{
-		printf("nil\n");
-	}
-	else
+	if ((*d).owner != NULL)
 	{
 		printf("Owner: %s\n", d->owner);
 	}
