8913f091dfd1c8ddef9fa9967dcaa4468036bcf3
GabrielCoh <9924@holbertonstudents.com>
2024-11-18 09:46:15 +0000

    prntname

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,17 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: function pointer casted to char pointer
+ *print_name - prints a name
+ *@name:name to print
+ *@f: function to print
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+
+	if (name == NULL || f == NULL)
+		return;
+
 	f(name);
+
 }
