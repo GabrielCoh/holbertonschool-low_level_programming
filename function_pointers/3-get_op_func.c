c8d7bde60da66906b4e2450330a5f34a93d5b211
GabrielCoh <9924holbertonstudents.com>
2024-11-21 10:59:22 +0000

    main.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,13 +1,6 @@
-#include "function_pointers.h"
-#include <stddef.h>
 #include "3-calc.h"
 
-/**
- * *get_op_func - function operator
- * @s: character
- * @i: integer
- * Return: function return
- */
+
 
 op_t ops[] = {
 	{"+", op_add},
@@ -18,16 +11,3 @@ op_t ops[] = {
 	{NULL, NULL}
 };
 int i;
-
-int (*get_op_func(char *s))(int, int)
-{
-	int i = 0;
-
-	while (ops[i].op != NULL)
-	{
-		if (s == ops[i].op && s[1] == '\0')
-			return (ops[i].f);
-		i++;
-	}
-	return (NULL);
-}
