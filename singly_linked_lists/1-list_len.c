0b5e1f9b6e8df88d8674784ca8623c3cc63762b8
GabrielCoh <9924@holbertonstudents.com>
2024-12-02 14:28:03 +0000

    list lenght of linked list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - Counts the number of elements in the list
+ * @h: pointer to the list
+ * Return: the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while(h)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
